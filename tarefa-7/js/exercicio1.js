8f6131efb68c81dddcbb71ffd904d6be6536f0ad
Alessandro <avirmond2000@gmail.com>
2022-02-14 13:42:34 -0300

    tarefa de js de strings com DOM adicionada

--- tarefa-7/js/exercicio1.js
+++ tarefa-7/js/exercicio1.js
@@ -1,6 +1,6 @@
-let texto = document.getElementById("texto");
-let paragrafo = document.getElementById("resultado");
-
-texto.addEventListener("keyup", function(){
-    paragrafo.innerHTML = texto.value;
-});
+function inverter(){
+    var inverter = document.getElementsByName("txt");
+    valor = inverter.item(0).value.toString().split("");
+    normal = valor.reverse().join("");
+    document.body.innerHTML += normal + "<br />";
+}
