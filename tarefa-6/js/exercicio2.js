7486ea062eff8ad8bef8d7b4db860e7bafc3d065
alessandrovirmond <avirmond2000@gmail.com>
2025-05-16 18:08:37 -0300

    fazendo minha pagina pessoal

--- tarefa-6/js/exercicio2.js
+++
@@ -1,7 +0,0 @@
-function potencias(num){
-    for(let i = 1; i <= 30; i++){
-        console.log(Math.pow(num, i));
-    }
-}
-
-potencias(4);
--- tarefa-7/js/exercicio1.js
+++
@@ -1,6 +0,0 @@
-function inverter(){
-    var inverter = document.getElementsByName("txt");
-    valor = inverter.item(0).value.toString().split("");
-    normal = valor.reverse().join("");
-    document.body.innerHTML += normal + "<br />";
-}
